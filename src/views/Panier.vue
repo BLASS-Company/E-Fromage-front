dc1edab6056dee7e930dc751989d5e06ff30ef7a
Benoit.Spriet <benoit.spriet@epitech.eu>
2020-12-02 15:34:06 +0100

    boutique et panier

--- src/views/Panier.vue
+++ src/views/Panier.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
-      {{vue_panier}}
+    <ul>
+      <li v-for="(produit, index) in vue_panier" :key="index">
+        <p>{{ produit.title }}</p>
+        <p>{{ produit.body }}</p>
+        <!-- <p>{{produit.name}}</p>
+        <p>{{produit.description}}</p>
+        <p>{{produit.prix}}</p>
+        <p>{{produit.stock}}</p> -->
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-    name: "Panier",
-    computed: {
-      vue_panier() {
-        return this.$store.state.produits_panier
-      }
-    }
-
-}
+  name: "Panier",
+  computed: {
+    vue_panier() {
+      return this.$store.state.produits_panier;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
