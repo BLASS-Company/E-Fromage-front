7cf5efece844d0717d49fda83d653340a8623091
AlexisResses <alexis.resses@epitech.eu>
2020-12-03 18:28:53 +0100

    accueil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,46 @@
 <template>
-<v-container>
-  <v-row>
-    <div class="selection">
-      <h3>Notre Selection</h3>
-      <p>Découvrez nos meilleurs fromages</p>
-      <v-btn @click="selection()">Voir plus</v-btn>
-      <v-col>
-      <v-img max-height="500" max-width="500" src="../assets/selection.jpg" alt="image de notre selection" />
+  <v-container>
+    <v-card id="select" height="500" class="d-flex">
+      <v-col cols="6" align-self="center">
+        <v-card-title>Notre Selection</v-card-title>
+        <v-card-text>Découvrez nos meilleurs fromages</v-card-text>
+        <v-btn @click="go_to_select()">Voir plus</v-btn>
       </v-col>
-    </div>
-  </v-row>
-    <div class="offer">
-      <h3>Nos plateaux de fromages</h3>
-      <p>Appréciez nos fromages à plusieurs</p>
-      <v-btn @click="plateau()">Voir plus</v-btn>
-      <v-img max-height="500" max-width="500" src="../assets/plate.jpg" alt="image de nos plateaux fromages" />
-    </div>
-    <div class="present">
-      <h3>Nos idées cadeaux</h3>
-      <p>Faites vous plaisir pendant les fêtes</p>
-      <v-btn @click="cadeau()">Voir plus</v-btn>
-      <v-img max-height="500" max-width="500" src="../assets/present.jpg" alt="image de nos idées cadeaux" />
-    </div>
-    <div class="about">
-      <h3>A propos des Pitech</h3>
-      <p>
-        Avec plus de 1000 brebis. Nous avons beaucoup de moment à partager avec
-        vous.
-      </p>
-      <p>
-        C'est pourquoi nous vous proposons de découvrir notre quotidien dans la
-        ferme des Pitech.
-      </p>
-      <button @click="go_to_about()">Découvrez-nous</button>
-    </div>
-    <div class="comment">
-      <p>"C'est le meilleur formage que j'ai jamais goûté de toute ma vie</p>
-      <p>Raphael D et Alexandre B ont commenté</p>
-    </div>
-</v-container>
+      <v-col align-self="center" cols="6">
+        <v-img height="500" src="../assets/selection.jpg" alt="image de notre selection" />
+      </v-col>
+    </v-card>
+    <v-card id="plate" height="500" class="d-flex">
+      <v-col align-self="center" cols="6">
+        <v-img height="500" src="../assets/plate.jpg" alt="image de nos plateaux fromages" />
+      </v-col>
+      <v-col cols="6" align-self="center">
+        <v-card-title>Nos plateaux de fromages</v-card-title>
+        <v-card-text>Appréciez nos fromages à plusieurs</v-card-text>
+        <v-btn @click="go_to_plate()">Voir plus</v-btn>
+      </v-col>
+    </v-card>
+    <v-card id="present" height="500" class="d-flex">
+      <v-col cols="6" align-self="center">
+        <v-card-title>Nos idées cadeaux</v-card-title>
+        <v-card-text>Faites vous plaisir pendant les fêtes</v-card-text>
+        <v-btn @click="go_to_present()">Voir plus</v-btn>
+      </v-col>
+      <v-col align-self="center" cols="6">
+        <v-img height="500" src="../assets/present.jpg" alt="image de nos idées cadeaux" />
+      </v-col>
+    </v-card>
+    <v-card id="about" height="300">
+      <v-card-title>A propos des Pitech</v-card-title>
+      <v-card-text> Avec plus de 1000 brebis. Nous avons beaucoup de moment à partager avec vous.</v-card-text>
+      <v-card-text> C'est pourquoi nous vous proposons de découvrir notre quotidien dans la ferme des Pitech. </v-card-text>
+      <v-btn @click="go_to_about()">Découvrez-nous</v-btn>
+    </v-card>
+    <v-card id="comment" height="200">
+      <v-card-text>"C'est le meilleur formage que j'ai jamais goûté de toute ma vie.</v-card-text>
+      <v-card-text>Raphael D et Alexandre B ont commenté</v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -58,11 +60,8 @@ export default {
     },
     go_to_about() {
       this.$router.push("/a_propos_de_nous");
-    }
-  }
+    },
+  },
 };
 </script>
-<style>
-
-
-</style>
+<style></style>
