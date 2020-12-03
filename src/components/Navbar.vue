ed08b7da20d23b3fe1720dbbe918447fcaaa1148
AlexisResses <alexis.resses@epitech.eu>
2020-12-03 12:24:22 +0100

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,21 @@
 <template>
   <v-app-bar>
+    <v-row align="start">
     <h2>LA FERME DES PITECH</h2>
+    </v-row>
+    <v-row align="center">
     <v-tab><router-link to="/">Accueil</router-link></v-tab>
     <v-tab><router-link to="/boutique/posts">Nos Produits</router-link></v-tab>
     <v-tab><router-link to="/contact">Contact</router-link></v-tab>
     <v-tab><router-link to="/a_propos">A Propos</router-link></v-tab>
+    </v-row>
     <v-tab>
       <v-icon @click="Panier()">fa-shopping-cart</v-icon>
       {{ paniercount }}
    </v-tab>
-    <v-tab @click="Sign_in()">Identifiez-vous</v-tab>
-    <v-tab @click="Sign_up()">Inscrivez-vous</v-tab>
+    <v-btn @click="Sign_in()">Identifiez-vous</v-btn>
+    <v-btn @click="Sign_up()">Inscrivez-vous</v-btn>
+    
   </v-app-bar>
 </template>
 
