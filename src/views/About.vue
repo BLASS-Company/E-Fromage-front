879ede32651368218518e21f1b2b6f6c994e39b4
Benoit.Spriet <benoit.spriet@epitech.eu>
2020-12-11 09:37:09 +0100

    image ok

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
       <v-card class="text-center">
         <v-card-title class="justify-center">La plus belle ferme du monde</v-card-title>
         <v-card-text>Depuis 1929, nous élevons nos brebis afin de réaliser le meilleur fromage de l’univers. </v-card-text>
-        <v-img aspect-ratio="1.5" src="../assets/farm.jpg" alt="image de la ferme des pitech" />
+        <v-img aspect-ratio="1.5" src="https://images.unsplash.com/photo-1444858291040-58f756a3bdd6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1530&q=80" alt="image de la ferme des pitech" />
       </v-card>
     </v-container>
   </v-main>
